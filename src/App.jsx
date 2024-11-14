46af3338b085e6345e29d5d113505c8b978c3d49
Dhruv0369 <148555616+Dhruv0369@users.noreply.github.com>
2024-11-14 18:57:02 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-// import Home from './components/home'
 import Theme from './components/theme'
 
 function App() {
 
   return (
     <>
-      <Theme/>
-      {/* <Home/> */}
+      <Theme />
     </>
   )
 }
